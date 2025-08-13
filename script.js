895719f80e5c478a7c93ef25209bc0c0d2be3aaa
MahadevDoshetty <mahadevdoshetty317@gmail.com>
2025-08-13 09:08:38 +0530

    Largest Number Commit

--- script.js
+++ script.js
@@ -2,24 +2,35 @@ console.log("Welcome to Dev's Dev Tools");
 // Q1. Anagarm function practice
 // Anagram is a word/phrase/something which is formed by rearranging the words;
 
-function isAnagram(str1, str2) {
-    let arr1 = str1.toLowerCase().split("").sort(); // Sort  and split *
-    let arr2 = str2.toLowerCase().split("").sort(); // To lowercase 
-    // console.log(arr1, "and", arr2);
-    if (arr1.length !== arr2.length) {
-        console.log("Words are of different length");
-        return false;
-    }
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-            console.log("No they are not Anagram ");
-            return false; // This is very useful in the loop, it short-circuits loop if any one value mis-match 
-        }
+// function isAnagram(str1, str2) {
+//     let arr1 = str1.toLowerCase().split("").sort(); // Sort  and split *
+//     let arr2 = str2.toLowerCase().split("").sort(); // To lowercase 
+//     // console.log(arr1, "and", arr2);
+//     if (arr1.length !== arr2.length) {
+//         console.log("Words are of different length");
+//         return false;
+//     }
+//     for (let i = 0; i < arr1.length; i++) {
+//         if (arr1[i] !== arr2[i]) {
+//             console.log("No they are not Anagram ");
+//             return false; // This is very useful in the loop, it short-circuits loop if any one value mis-match 
+//         }
 
-    }
-    console.log("Yes they are Anagram ")
-    return true;
+//     }
+//     console.log("Yes they are Anagram ")
+//     return true;
 
 
-};
-isAnagram("mahxadev", "mahadevx");
+// };
+// isAnagram("mahxadev", "mahadevx");
+
+//Q2. Find the largest number from an array and return it;
+
+function isLargestNum(arr1) {
+    let sortedArray1 = arr1.sort(function (a, b) { return a - b; });
+    console.log(Number(sortedArray1));
+    console.log(sortedArray1[sortedArray1.length - 1 ], "is the largest number among the array");
+}
+isLargestNum([1, 2, 33, 4, 5, 99,125,131,145]);
+
+
